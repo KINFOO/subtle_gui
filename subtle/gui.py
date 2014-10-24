daffdc491b50ae6950ab324b95673ad75be17e2b
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2014-10-24 14:21:13 +0200

    More elegant display

--- subtle/gui.py
+++ subtle/gui.py
@@ -1,13 +1,16 @@
 from PyQt5.QtWidgets import QMainWindow, QTreeWidget, QTreeWidgetItem
+from PyQt5.QtCore import QUrl
+
+import os
 
 __all__ = ['SubtleMainWindow',]
 
 class SubtleMainWindow(QMainWindow):
 
-    def __init__(self, items=[]):
+    def __init__(self, paths=[]):
         QMainWindow.__init__( self )
         self.setWindowTitle( 'Subtle' )
-        self._tree = SubtleTreeWidget(self, items)
+        self._tree = SubtleTreeWidget(self, paths_to_items_list(self, paths))
         self.setCentralWidget( self._tree )
         self.setAcceptDrops( True )
 
@@ -15,8 +18,8 @@ class SubtleMainWindow(QMainWindow):
         event.acceptProposedAction()
 
     def dropEvent(self, event):
-        self._tree.addTopLevelItems([ QTreeWidgetItem(self._tree, [url.path()])
-            for url in event.mimeData().urls() ])
+        items = paths_to_items_list(self._tree, event.mimeData().urls())
+        self._tree.addTopLevelItems( items )
         event.acceptProposedAction()
 
 class SubtleTreeWidget(QTreeWidget):
@@ -25,4 +28,14 @@ class SubtleTreeWidget(QTreeWidget):
         QTreeWidget.__init__( self )
         self.setColumnCount( 1 )
         self.setHeaderHidden( True )
-        self.insertTopLevelItems(0, items)
+        self.addTopLevelItems( items )
+
+def paths_to_items_list(parent, paths):
+    items = []
+    for path in paths:
+        path = QUrl(path) if type(path) == QUrl else path
+        item = QTreeWidgetItem(parent, [ path.path() ])
+        item.setData(0, 0, path)
+        item.setText(0, os.path.basename(path.fileName()))
+        items.append( item )
+    return items
